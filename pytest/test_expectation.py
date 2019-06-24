a5903d68ddc41b1f18de5e3e2a467bd31003aca2
sevencrime <onedi@qq.com>
2019-06-24 18:36:29 +0800

    待研究

--- pytest/test_expectation.py
+++ pytest/test_expectation.py
@@ -1,15 +1,31 @@
 # content of test_expectation.py
 
 # coding:utf-8
-
 import pytest
-@pytest.mark.parametrize("test_input,expected",
-                         [ ("3+5", 8),
-                           ("2+4", 6),
-                           ("6 * 9", 42),
-                         ])
-def test_eval(test_input, expected, flag):
-    assert eval(test_input) == expected
+
+class Test:
+    def RiskTolerance(num=None):
+
+        def wrapper(func):
+            def inner_wrapper(self, *args, **kwargs):
+                print("正在执行用例 :", func.__name__)
+                print(*args, **kwargs, "sssssssssssssssssssssss")
+                return func(self, *args, **kwargs)
+            return inner_wrapper
+        return wrapper
+
+
+
+
+
+    @RiskTolerance(num=1)
+    @pytest.mark.parametrize("test_input,expected",
+                             [ ("3+5", 8),
+                               ("2+4", 6),
+                               ("6 * 9", 42),
+                             ])
+    def test_eval(self, test_input, expected):
+        assert eval(test_input) == expected
 
 if __name__ == "__main__":
     # pytest.main(["-s", "test_expectation.py", "--html=report.html"])
